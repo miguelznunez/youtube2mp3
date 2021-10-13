98ffbaaa3c21f650d5b64a580c4e83ba97483602
Code Foxx <iamcodefoxx@gmail.com>
2021-10-13 15:56:08 -0700

    update

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const express = require("express");
 const fetch = require("node-fetch");
 const https = require('https');
 const fs = require('fs');
+const bodyParser = require("body-parser")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,47 +15,57 @@ const PORT = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { success : true});
 });
 
-app.get('/downloadSound', async (req, res) => {
+app.post("/downloadSound", async (req, res) => {
+  // Check if the video id is empty
   if(
-    req.query.videoId === undefined ||
-    req.query.videoId === "" ||
-    req.query.videoId === null
+    req.body.videoId === undefined ||
+    req.body.videoId === "" ||
+    req.body.videoId === null
   ){
-    return res.json({"success":false, "msg":"Please add a video ID"});
+  //return an error if its empty
+    return res.json({ success :false, msg :"Please enter a video ID"});
   }
+  else{
+    // if its not empty the api call
+    const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${req.body.videoId}`, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": process.env.API_KEY,
+      "x-rapidapi-host": process.env.API_HOST
+      }
+    });
 
-  const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${req.query.videoId}`, {
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-key": process.env.API_KEY,
-    "x-rapidapi-host": process.env.API_HOST
-    }
-  });
-
-  const fetchResponse = await fetchAPI.json();
-
-  // downloads directly on browser
-  res.redirect(fetchResponse.link);
-  
-  // downloads to a folder
-  // https.get(fetchResponse.link, (res) => {
-  //   const path = "downloaded-image.mp3";
-  //   const writeStream = fs.createWriteStream(path);
-
-  //   res.pipe(writeStream);
-
-  //   writeStream.on("finish", () => {
-  //     writeStream.close();
-  //     console.log("Download Completed");
-  //   });
-  // });
-  
+    const fetchResponse = await fetchAPI.json();
+    // check for errors
+    if(fetchResponse.status === "ok")
+      return res.json({ success : true, msg : fetchResponse.link, title : fetchResponse.title}) 
+    else
+      return res.json({ success : false, msg : "Please enter a valid ID"});
+  }
 });
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
+// downloads to a folder
+    // https.get(fetchResponse.link, (res) => {
+    //   const path = "downloaded-image.mp3";
+    //   const writeStream = fs.createWriteStream(path);
+
+    //   res.pipe(writeStream);
+
+    //   writeStream.on("finish", () => {
+    //     writeStream.close();
+    //     console.log("Download Completed");
+    //   });
+    // });
--- public/js/functions.js
+++ public/js/functions.js
@@ -1,21 +1,41 @@
-document.querySelector("#download_form").addEventListener("submit", (e) => {
+
+document.querySelector("#contact-form").addEventListener("submit", (e) => {
   e.preventDefault();
 
   const videoId = document.querySelector("#videoId").value;
-
-  fetch("/downloadSound", {
-    method: "GET",
+  fetch('/downloadSound', {
+    method: "POST",
     headers: {
       "Accept": "application/json, text/plain, */*",
       "Content-type": "application/json"
-    }
+    },
+    body: JSON.stringify({ videoId: videoId })
   })
   .then((res) => res.json())
   .then((data) => {
-    if(!data.success)
-      document.querySelector("#download-error").textContent = data.msg;
-    else{
+    if(data.success === true){
+      // remove any error messages
+      document.querySelector("#error").style.display = "none";
+      // add song title
+      document.querySelector("#song-title").textContent = data.title;
+      // set the href to the url for the download
+      document.querySelector("#song-url").style.display= "block";
+      var a = document.querySelector("#song-url");
+      a.href = data.msg;
+      a.textContent = "DOWNLOAD MP3";
+      // clear the fields
       document.querySelector("#videoId").value = "";
+      document.querySelector("#error").textContent = "";
+    }
+    else{
+      // display error
+      document.querySelector("#error").style.display = "block";
+      document.querySelector("#error").textContent = data.msg;
+      // remove song title if any
+      document.querySelector("#song-title").textContent = "";
+      // block the download link from appearing
+      document.querySelector("#song-url").style.display= "none";      
     }
   });
+
 });
